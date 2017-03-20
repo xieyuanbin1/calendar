7d0822cdd84de925ced201b87ab0e7e41150914d
Nic <xybin1990@users.noreply.github.com>
2017-03-20 09:57:21 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
 * @1900-2100区间内的公历、农历互转
 * @charset  UTF-8
-* @Author  Jea杨(JJonline@JJonline.Cn) 
+* @Author  Jea杨(JJonline@JJonline.cn) 
 * @Time    2014-7-21
 * @Time    2016-8-13 Fixed 2033hex、Attribution Annals
 * @Version 1.0.1
