e008619e9c069edcdbe13e7908f5a00085c793c7
Xieyuanbin <xieyuanbin@acoinfo.com>
2022-06-17 19:04:55 +0800

    add types

---
+++ index.d.ts
@@ -0,0 +1,27 @@
+declare module 'solar2lunar' {
+  interface Calendar {
+    lYear: number;
+    lMonth: number;
+    lDay: number;
+    Animal: string;
+    IMonthCn: string;
+    IDayCn: string;
+    cYear: number;
+    cMonth: number;
+    cDay: number;
+    gzYear: string;
+    gzMonth: string;
+    gzDay: string;
+    isToday: boolean;
+    isLeap: boolean;
+    nWeek: number;
+    ncWeek: string;
+    isTerm: boolean;
+    Term: string;
+    astro: string;
+  }
+  export function solar2lunar(): Calendar;
+  export function solar2lunar(y: number, m: number, d: number): Calendar;
+  export function lunar2solar(): Calendar;
+  export function lunar2solar(y: number, m: number, d: number): Calendar;
+}
