d8dcb80e0dcf5ac88df8f7ff1671a325e13229fa
Xieyuanbin <xieyuanbin@acoinfo.com>
2022-06-18 13:43:53 +0800

    update exports

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
-;(function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
-    typeof define === 'function' && define.amd ? define(factory) :
-      global.calendar = factory()
-}(this, (function () {
+;(function (root, factory) {
+  if (typeof define === 'function' && define.amd) {
+    // AMD. Register as an anonymous module.
+    define([], factory);
+} else if (typeof module === 'object' && module.exports) {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory();
+} else {
+    // Browser globals (root is window)
+    root.returnExports = factory();
+}
+}(typeof self !== 'undefined' ? self : this, function () {
   'use strict';
 
   /**
@@ -531,4 +540,4 @@
     lunar2solar
   };
 
-})));
+}));
